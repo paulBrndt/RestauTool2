3db54b297a7fea68d2baeee4d21dd83cc40b788e
einsaetze-finanzen <138028176+einsaetze-finanzen@users.noreply.github.com>
2023-07-10 19:46:58 +0200

    Makes easier to use RegistrierenFeld.swift

--- Sources/RestauTool/RegistrierenFeld.swift
+++ Sources/RestauTool/RegistrierenFeld.swift
@@ -10,23 +10,25 @@ import SwiftUI
 public struct RegistrierenFeld: View{
     @State private var feld: RegistrierenFeldAuftrag
     @State private var placeholder: String?
-    @Binding private var text: String
+    @Binding var text: String
+    @Binding var data: RegistrierenData
     
-    public init(feld: RegistrierenFeldAuftrag, text: Binding<String>, platzhalter: String? = nil) {
+    public init(feld: RegistrierenFeldAuftrag, text: Binding<String>, platzhalter: String? = nil, data: Binding<RegistrierenData>) {
         self.feld = feld
         self._text = text
         self.placeholder = platzhalter
+        self._data = data
     }
     
     public var body: some View{
         if feld == .email{
-            TextFeld(icon: "envelope", placeholder: placeholder ?? "Email hier eingeben", text: $text)
+            TextFeld(icon: "envelope", placeholder: placeholder ?? "Email hier eingeben", text: $data.email)
         } else if feld == .name{
-            TextFeld(icon: "person", placeholder: placeholder ?? "Name hier eingeben", text: $text)
+            TextFeld(icon: "person", placeholder: placeholder ?? "Name hier eingeben", text: $data.name)
         } else if feld == .username{
-            TextFeld(icon: "fork.knife", placeholder: placeholder ?? "Öffentlichen Benutzernamen für andere Restaurants hier eingeben", text: $text)
+            TextFeld(icon: "fork.knife", placeholder: placeholder ?? "Öffentlichen Benutzernamen für andere Restaurants hier eingeben", text: $data.username)
         } else if feld == .password{
-            PasswordTextfeld(passwort: $text)
+            PasswordTextfeld(passwort: $data.password)
         } else {
             EmptyView()
         }
@@ -35,7 +37,7 @@ public struct RegistrierenFeld: View{
 
 
 
-public enum RegistrierenFeldAuftrag: CaseIterable{
+public enum RegistrierenFeldAuftrag: CaseIterable, Identifiable{
     case email
     case username
     case name
@@ -65,5 +67,9 @@ public enum RegistrierenFeldAuftrag: CaseIterable{
         case .password:
             return ""
         }
+    }
+    
+    public var id: UUID{
+        return UUID()
     }
 }
