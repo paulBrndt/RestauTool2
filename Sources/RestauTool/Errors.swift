0590f9c1cb4815103b32f52595fb568eed238a7e
einsaetze-finanzen <138028176+einsaetze-finanzen@users.noreply.github.com>
2023-07-11 14:14:14 +0200

    Error updating

--- Sources/RestauTool/Errors.swift
+++ Sources/RestauTool/Errors.swift
@@ -11,6 +11,7 @@ import SwiftUI
 public enum ReservationError: Error, LocalizedError{
     case schonBelegt
     case zuWeitImVoraus(abWannBuchbar: Date?)
+    case istInVergangenheit
     
     public var errorMessage: String{
         switch self {
@@ -37,6 +38,8 @@ public enum ReservationError: Error, LocalizedError{
                     "Leider ist dieser Termin noch zu weit entfernt. Probieren sie bitte ein näherliegendes Datum oder probieren sie es demnächst nocheinmal",
                     comment: "Zu Weit in der zukunft")
             }
+        case .istInVergangenheit:
+            return NSLocalizedString("Leider ist dieser Termin in der Vergangenheit. Wolange sie nicht Zeitreisen können ist es nicht möglich diesen Tisch zu reservieren (-:", comment: "Ist in Vergangenheit")
         }
     }
 }
