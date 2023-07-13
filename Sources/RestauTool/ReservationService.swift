62cce4c0b7e5338588dcbba9f64b0c60c1b4f503
einsaetze-finanzen <138028176+einsaetze-finanzen@users.noreply.github.com>
2023-07-13 17:35:18 +0200

    Reservation Update

--- Sources/RestauTool/ReservationService.swift
+++ Sources/RestauTool/ReservationService.swift
@@ -7,12 +7,16 @@
 
 import SwiftUI
 import Foundation
+import Firebase
+import FirebaseFirestoreSwift
 
 struct ReservationService{
     
     func daysBetween(start: Date, end: Date) -> Int {
             return Calendar.current.dateComponents([.day], from: start, to: end).day!
     }
+    
+    
     
     func findTableForReservation(date: Date, people: Int) throws -> String {
         #warning("Falls kein Tisch verfügbar ist ReservationError.schonBelegt benutzen")
@@ -21,8 +25,14 @@ struct ReservationService{
         return ""
     }
     
-    func addReservation(at date: Date, withHowManyPeople people: Int) throws {
-        if daysBetween(start: Date(), end: date) < 366{
+    
+    
+    
+    func addReservation(at date: Date, withHowManyPeople people: Int, nameOfGuests: String) throws {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        #warning("Error message erstellen")
+        
+        if daysBetween(start: Date(), end: date) > 366{
             throw ReservationError.zuWeitImVoraus(abWannBuchbar: Calendar.current.date(byAdding: .year, value: 1, to: Date()))
         }
         
@@ -35,10 +45,33 @@ struct ReservationService{
         }
         
         if let tableUid = tableUid{
-            
+            let id = UUID().uuidString
+            Firestore.firestore().collection("users").document(uid)
+                .collection("tables")
+                .document(tableUid)
+                .collection("reservations")
+                .document(id)
+                .setData(["id" : id,
+                 "nameOfGuest" : nameOfGuests,
+                          "date" : Timestamp(date: date)])
         }
-        
-        
+    }
+    
+    
+    func fetchReservations(forTable tableUid: String, completion: @escaping([Reservierung]) -> Void){
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        Firestore.firestore().collection("users").document(uid)
+            .collection("tables")
+            .document(tableUid)
+            .collection("reservations")
+            .getDocuments { snapshot, error in
+                if let error = error{
+                    print(error.localizedDescription)
+                    return
+                }
+                guard let docs = snapshot?.documents else { return }
+                let reservations = docs.compactMap({ try? $0.data(as: Reservierung.self) })
+            }
     }
     
 }
